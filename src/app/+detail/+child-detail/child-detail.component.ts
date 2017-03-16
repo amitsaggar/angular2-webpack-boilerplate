f1e9f5a59ff5176bf5899242221e2e7ec7c2483c
Amit Saggar <amit.saggar@move.com>
2017-03-16 16:55:45 -0700

    small update to test git

--- src/app/+detail/+child-detail/child-detail.component.ts
+++ src/app/+detail/+child-detail/child-detail.component.ts
@@ -5,7 +5,7 @@ import {
 /*
  * We're loading this component asynchronously
  * We are using some magic with es6-promise-loader that will wrap the module with a Promise
- * see https://github.com/gdi2290/es6-promise-loader for more info
+ * see https://github.com/gdi2290/es6-promise-loader for more info. This will give us more details
  */
 
 console.log('`ChildDetail` component loaded asynchronously');
